399c2dcf7d249cafafa4dc502e7536b7cdd9892f
Sharafat Ahmed Sabir <asabir24@gmail.com>
2021-07-30 20:15:50 +0600

    Added solution to `two sum` problem

---
+++ TwoSum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: 'List[int]', target: 'int') -> 'List[int]':
+        checker = {}
+        for i, v in enumerate(nums):
+            if target - v in checker:
+                return [checker[target - v], i]
+            else:    
+                checker[v] = i
+        return []
